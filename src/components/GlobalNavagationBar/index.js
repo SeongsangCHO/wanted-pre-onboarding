2c1ac972ce928fd347ccc38b50d4393fe4345d32
Seongsangcho <liasqui31@gmail.com>
2021-07-14 18:49:09 +0900

    init

---
+++ src/components/GlobalNavagationBar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const GlobalNavationBar = props => {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+};
+
+GlobalNavationBar.propTypes = {
+  
+};
+
+export default GlobalNavationBar;
