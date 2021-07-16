17ce5e2a075b2eb165c25d78d26f10939fd600e7
Seongsangcho <liasqui31@gmail.com>
2021-07-16 16:47:19 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import GlobalNavationBar from "./components/GlobalNavagationBar";
 import { exploreItems, exploreTitleItems, leftMenuItems } from "./data/data";
