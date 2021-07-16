a68d59adac707efb596b370ca561b52885f460dc
Seongsangcho <liasqui31@gmail.com>
2021-07-16 15:47:33 +0900

    [refactor] 컴포넌트 분리

---
+++ src/components/ExploreMenuDropDown/ExploreMenuOnlyTitles/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ExploreMenuCategoryContainer } from "../style";
+
+const ExploreMenuOnlyTitles = (props) => {
+  const { exploreTitleItems } = props;
+
+  return (
+    <ExploreMenuCategoryContainer>
+      {exploreTitleItems?.map((item) => {
+        return (
+          <a key={item + "title"}>
+            <h2>{item}</h2>
+            <span className="arrow-right">{">"}</span>
+          </a>
+        );
+      })}
+    </ExploreMenuCategoryContainer>
+  );
+};
+
+ExploreMenuOnlyTitles.propTypes = {};
+
+export default ExploreMenuOnlyTitles;
